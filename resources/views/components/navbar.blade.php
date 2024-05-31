5a6a547477171a6a5f4f52c3da5dc699482b6670
Eugene702 <eugenefeilianputrarangga@gmail.com>
2024-05-31 19:23:33 +0700

    Feat: Masa pembuatan landing page

---
+++ resources/views/components/navbar.blade.php
@@ -0,0 +1,14 @@
+<div>
+    <div class="navbar absolute top-0 text-white w-10/12 left-2/4 transform -translate-x-2/4">
+        <div class="flex-1">
+          <a class="btn btn-ghost text-xl rounded-box">Bima Sakti</a>
+        </div>
+        <div class="flex-none">
+          <ul class="menu menu-horizontal px-1">
+            <li><a href="/">Beranda</a></li>
+            <li><a href="/contact">Kontak</a></li>
+            <li><a href="/about">Tentang Kami</a></li>
+          </ul>
+        </div>
+      </div>
+</div>
---
+++ resources/views/home.blade.php
@@ -0,0 +1,35 @@
+<x-layouts.defaults>
+    <main>
+        <div class="hero min-h-screen"
+            style="background-image: url(https://images.unsplash.com/photo-1522202176988-66273c2fd55f?q=80&w=2071&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D;">
+            <div class="hero-overlay bg-opacity-60"></div>
+            <div class="hero-content w-full justify-start text-neutral-content">
+                <div class="max-w-md">
+                    <h1 class="mb-5 text-5xl font-bold">Menciptakan Masa Depan dengan Inovasi Tanpa Batas</h1>
+                    <p>Bima Sakti Tech memimpin inovasi teknologi untuk masa depan yang lebih terhubung dan efisien.
+                        Bergabunglah dengan kami untuk membuka peluang tanpa batas.</p>
+                    <button class="btn btn-primary rounded-full mt-5 uppercase">Pelajari Lebih Lanjut</button>
+                </div>
+            </div>
+        </div>
+
+        <div class="hero min-h-[calc(100vh-20rem)]">
+            <div class="hero-content text-center uppercase">
+                <div>
+                    <h1 class="text-2xl">Inovasi Global dengan Akar Lokal</h1>
+                    <h1 class="text-6xl font-bold">Di Mana Kami Beroperasi</h1>
+                    <a href="/about" class="btn btn-primary mt-5 rounded-full uppercase">Cari tahu lebih lanjut</a>
+                </div>
+            </div>
+        </div>
+
+        <div class="grid grid-cols-2 gap-x-10 pl-10 bg-base-200">
+            <div class="flex flex-col justify-center">
+                <h1 class="text-2xl mb-3 font-bold">Percaya pada Karya yang Baik</h1>
+                <h1 class="text-4xl tracking-[.25rem] font-light">Kami percaya dalam menjalankan bisnis secara terbuka, etis, dan berkontribusi kembali kepada komunitas yang kami layani. Di Bima Sakti Tech, kami berkomitmen untuk menciptakan dampak positif melalui inovasi teknologi dan tanggung jawab sosial.</h1>
+            </div>
+
+            <img src="https://images.unsplash.com/photo-1485827404703-89b55fcc595e?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="">
+        </div>
+    </main>
+</x-layouts.defaults>
